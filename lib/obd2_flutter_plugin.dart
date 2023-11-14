1cfd860b428c13a7f6ba7d639c15185686d7ef1b
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-11-14 17:28:57 +0200

    feat: fully implemented Obd2FlutterPlugin for flutter side

--- lib/obd2_flutter_plugin.dart
+++ lib/obd2_flutter_plugin.dart
@@ -1,11 +1,19 @@
 import 'obd2_flutter_plugin_platform_interface.dart';
 
 class Obd2FlutterPlugin {
-  Future<int?> getFuelLevel() {
-    return Obd2FlutterPluginPlatform.instance.getFuelLevel();
+  Future<List<String>?> getBluetoothDevices() {
+    return Obd2FlutterPluginPlatform.instance.getBLEDevices();
   }
 
-  // Future<Map<String, String>?> connectOBD() {
-  //   return Obd2FlutterPluginPlatform.instance.connectOBD();
-  // }
+  Future<bool?> connect(String address) {
+    return Obd2FlutterPluginPlatform.instance.connect(address);
+  }
+
+  Future<void> init() {
+    return Obd2FlutterPluginPlatform.instance.init();
+  }
+
+  Future<String?> getFuelLevel() {
+    return Obd2FlutterPluginPlatform.instance.getFuelLevel();
+  }
 }
--- lib/obd2_flutter_plugin_method_channel.dart
+++ lib/obd2_flutter_plugin_method_channel.dart
@@ -5,12 +5,30 @@ import 'obd2_flutter_plugin_constants.dart';
 import 'obd2_flutter_plugin_platform_interface.dart';
 
 class MethodChannelObd2FlutterPlugin extends Obd2FlutterPluginPlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel(METHOD_CHANNEL_NAME);
+  final bleChannel = const MethodChannel(FUEL_CHANNEL);
+
+  @visibleForTesting
+  final fuelChannel = const MethodChannel(BLUE_DEVICES_CHANNEL);
 
   @override
-  Future<int?> getFuelLevel() async {
-    return await methodChannel.invokeMethod<int?>(GET_FUEL_LEVEL_METHOD_NAME);
+  Future<List<String>?> getBLEDevices() async {
+    return await bleChannel
+        .invokeMethod<List<String>?>(SCAN_BLE_DEVICES_METHOD_NAME);
+  }
+
+  @override
+  Future<bool?> connect(String address) async {
+    return await fuelChannel.invokeMethod<bool?>(CONNECT_OBD_METHOD_NAME);
+  }
+
+  @override
+  Future<void> init() async {
+    return await fuelChannel.invokeMethod<void>(INIT_OBD_METHOD_NAME);
+  }
+
+  @override
+  Future<String?> getFuelLevel() async {
+    return await fuelChannel.invokeMethod<String?>(GET_FUEL_LEVEL_METHOD_NAME);
   }
 }
