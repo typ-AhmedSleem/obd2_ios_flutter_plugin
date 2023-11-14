a00a3e36cc83d8a66305b4a4c7a473dadf8d0434
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-11-14 17:27:08 +0200

    feat: finished all of Obd2FlutterPlugin logic

--- ios/Classes/Obd2FlutterPlugin.swift
+++ ios/Classes/Obd2FlutterPlugin.swift
@@ -3,52 +3,68 @@ import UIKit
 
 public class Obd2FlutterPlugin: NSObject, FlutterPlugin {
 
-  @ObservedObject private var obd2 = OBD2()
+  private var obd2 = OBD2()
 
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: Constants.METHOD_CHANNEL_NAME, binaryMessenger: registrar.messenger())
+    let bluetoothChannel = FlutterMethodChannel(name: MethodChannelsNames.BLUE_DEVICES, binaryMessenger: registrar.messenger())
+    let fuelChannel = FlutterMethodChannel(name: MethodChannelsNames.FUEL, binaryMessenger: registrar.messenger())
     let instance = Obd2FlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addMethodCallDelegate(instance, channel: bluetoothChannel)
+    registrar.addMethodCallDelegate(instance, channel: fuelChannel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case Constants.GET_FUEL_LEVEL_METHOD_NAME:
-      result(self.getCarFuelLevel())
-    case Constants.CONNECT_OBD_METHOD_NAME:
-        // todo: Check Bluetooth.
-        // todo: Connect to OBD2.
-        // todo: Get Peripherals of OBD2 to start communication.
-      result(FlutterMethodNotImplemented)
-    case Constants.INIT_OBD2_METHOD_NAME:
-      // todo: Send (init) commands sequence in-order to initialize the OBD
-      // todo: Return the result code and tunnel it back to Flutter to update UI
-      result(FlutterMethodNotImplemented)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+      case MethodsNames.SCAN_BLUETOOTH_DEVICES:
+        if self.obd2.isBLEManagerInitialized {
+          do {
+            let devices: [String] = await obd2.bluetoothManager.retrieveBoundedBluetoothDevicesSerialized()
+            result(devices)
+          } catch {
+            let emptyDevicesList: [String] = [] 
+            result(emptyDevicesList)
+          }
+        }
+        //* If we reached this point, it means that BLE manager hasn't yet been initialized. So, result an error
+        result(FlutterError(
+          code: "400",
+          message: "BluetoothManager either poweredOff or hasn't yet been initialized yet. Check it in your settings.",
+          details: nil
+        ))
+      case MethodsNames.CONNECT_ADAPTER:
+        do {
+          //? Flutter will send me device address in args
+          let address = call.arguments as String? else { throw CantConnectError() }
+          let connected = await self.obd2.connect(target: address)
+          result(connected)
+        } catch {
+            result(FlutterError(
+              code: "400",
+              message: "Can't connect to device. Please try again",
+              details: nil
+            ))
+        }
+      case MethodsNames.INIT_ADAPTER:
+        do {
+          await self.obd2.initializeOBD()
+          //* This method result nothing and idk why :)
+        } catch {
+          result(FlutterError(
+            code: "400",
+            message: "Can't initialize the OBD adapter. Check if it's connected or it may be out-of-range",
+            details: nil
+          ))
+        }
+      case MethodsNames.GET_FUEL_LEVEL:
+        do {
+          let fuelLevel = await obd2.executeCommand(command: FuelLevelCommand(delay: 100), expectResponse: true)
+          result(fuelLevel)
+        } catch {
+          result(-1)
+        }
+      default:
+        result(FlutterMethodNotImplemented)
+      }
   }
-
-  private func connectOBD() -> Bool {
-    // todo: Code this method to check for Bluetooth stuff like (Permission, Enable and Hardware)
-    return false
-  }
-
-  private func initOBD() -> Bool {
-    // todo: Send the sequence of INIT commands to the OBD adapter
-    return false
-  }
-
-  private func sendCommand(command: ODBCommand) -> Void {
-  }
-
-  private func getCarFuelLevel() -> Int {
-    // todo: Send ACK command to OBD2.
-    // todo: Send GetFuelLevel command to OBD2.
-    // todo: Wait for the result from OBD2.
-    var carFuelLevel = self.INITIAL_RESULT
-    
-    return carFuelLevel
-  } 
 
 }
