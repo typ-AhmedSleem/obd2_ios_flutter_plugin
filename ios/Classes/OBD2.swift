f9dc4ba420550225f9b0867bd57e794e26f16e1e
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-01-08 17:36:41 +0200

    impr: made more improvements on OBD2 class

--- ios/Classes/OBD2.swift
+++ ios/Classes/OBD2.swift
@@ -8,90 +8,90 @@
 import Foundation
 
 class OBD2 : NSObject {
-
+    
+    // todo: Use executionQueue for async-wait public functions instead of using Tasks
+    
     //* Global runtime
     private let logger = Logger("OBD2")
-    var delegate: OBD2Delegate?
+    private var delegate: OBD2Delegate?
+    private var lastFuelLevel: String = "--%"
     private let executionQueue = DispatchQueue(label: "com.typ.obd.OBD2Queue")
-
+    
     //* BluetoothManager runtime
     public var bluetoothManager: BluetoothManager?
     public var isBLEManagerInitialized: Bool {
         get {
-            return self.bluetoothManager != nil && self.bluetoothManager!.isInitialized
+            // Check if bluetooth manager is nil
+            if self.bluetoothManager == nil {
+                return false
+            }
+            // Check if bluetooth manager is not yet powered on
+            if !self.bluetoothManager!.isPowredOn {
+                return false
+            }
+            // Check if bluetooth manager has at least one open channel for writing
+            return self.bluetoothManager?.hasOpenWritingChannel ?? false
         }
     }
-
-
+    
     override init() {
         super.init()
         self.delegate = self
         self.initBluetoothManager()
         logger.log("Created a new OBD2 instance.")
     }
-
+    
     public func initBluetoothManager() {
         if self.bluetoothManager == nil {
             self.bluetoothManager = BluetoothManager(delegate: self)
         }
     }
-
+    
     public func connect(target address: String) async -> Bool {
-        guard let bm = self.bluetoothManager else { return false }
-        guard bm.isInitialized else { 
-            logger.log("BluetoothManager hasn't yet initialized")
+        guard self.isBLEManagerInitialized else {
+            logger.log("connect", "BluetoothManager hasn't yet initialized")
             return false
         }
-        return await bm.connect(target: address)
+        return await bluetoothManager?.connect(target: address) ?? false
     }
-
+    
     /**
-    * Executes sequence of commands to initialize the OBD adapter after successfully connecting to it
-    */
+     * Executes sequence of commands to initialize the OBD adapter after successfully connecting to it
+     */
     public func initializeOBD() async {
-        logger.log("Initializing adapter...")
-        //self.executionQueue.sync  {
-            let initialCommands = [
-                EchoOffCommand(),
-                LineFeedOffCommand(),
-                TimeoutCommand(timeout: 100),
-                SelectProtocolCommand(obdProtocol: ObdProtocols.AUTO)
-            ]
-            for command in initialCommands {
-                logger.log("(initializeOBD): Executing '\(command.cmd)' ...")
-                await self.executeCommand(command, expectResponse: false)
-            }
-       // }
+        logger.log("initializeOBD", "Initializing adapter...")
+        let initializeCommands = [
+            EchoOffCommand(),
+            LineFeedOffCommand(),
+            TimeoutCommand(timeout: 100),
+            SelectProtocolCommand(obdProtocol: ObdProtocols.AUTO)
+        ]
+        for command in initializeCommands {
+            logger.log("initializeOBD", "Executing '\(command.cmd)' ...")
+            _ = await self.executeCommand(command, expectResponse: false)
+        }
+        self.logger.log("initializeOBD", "OBD2 adapter is now initialized.")
     }
     
     /** Call the command execution call in a async block and await for result if expectResponse is true */
-    public func executeCommand(_ command: ObdCommand?, expectResponse: Bool) async -> String? {
-        if let command = command {
-            //self.executionQueue.sync {
-            if let bm = self.bluetoothManager {
-                if !bm.isChannelOpened {
-                    logger.log("Channel is closed")
-                    return nil
-                }
-                self.delegate?.onCommandExecuted(command, hasResponse: expectResponse)
-                return await command.execute(bleManager: bm, expectResponse: expectResponse)
-            } else {
-                return nil
-            }
-            //}
+    public func executeCommand(_ command: ObdCommand, expectResponse: Bool) async -> String? {
+        if let bm = self.bluetoothManager {
+            return await command.execute(bleManager: bm, expectResponse: expectResponse)
         } else {
-            logger.log("Command is nil")
             return nil
         }
     }
     
     public func getFuelLevel() async -> String? {
         let fuelLevel = await self.executeCommand(FuelLevelCommand(delay: 100), expectResponse: true)
-        
-        
-        return fuelLevel
+        if let newFuelLevel = fuelLevel {
+            self.lastFuelLevel = newFuelLevel
+            return newFuelLevel
+        } else {
+            return self.lastFuelLevel
+        }
     }
-
+    
 }
 
 protocol OBD2Delegate {
@@ -101,8 +101,14 @@ protocol OBD2Delegate {
 
 
 extension OBD2: BluetoothManagerDelegate {
+    
     func onAdapterConnected() {
-        logger.log("Adapter connected.")
+        logger.log("onAdapterConnected", "Adapter connected.")
+        Task {
+            // Initialize adapter
+            await self.initializeOBD()
+            self.onAdapterInitialized()
+        }
     }
     
     func onAdapterInitialized() {
@@ -120,18 +126,16 @@ extension OBD2: BluetoothManagerDelegate {
     func onAdapterReceiveResponse(response: String?) {
         logger.log("Adapter received response: \(String(describing: response))")
     }
-
+    
 }
 
 
 extension OBD2: OBD2Delegate {
+    
     func onCommandExecuted(_ command: ObdCommand, hasResponse: Bool) {
-        // logger.log("Command \(command.cmd) executed")
     }
     
     func onResponseReceived(_ command: ObdCommand, response: String?) {
-        
     }
-    
     
 }
