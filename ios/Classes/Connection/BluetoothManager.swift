30461017c164a8940ff534c2de7462998f2d9965
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-01-08 17:33:20 +0200

    impr: improved connection and added better features to BluetoothManager

--- ios/Classes/Connection/BluetoothManager.swift
+++ ios/Classes/Connection/BluetoothManager.swift
@@ -2,96 +2,129 @@ import Foundation
 import CoreBluetooth
 
 class BluetoothManager : NSObject {
-
+    
     //* Global runtime
-    private let logger = Logger("BLEManager")
+    private let logger = Logger("BM")
     private let responseStation =  ResponseStation(queueSize: 50)
     private var delegate: BluetoothManagerDelegate?
     private var boundedDevices: [String: CBPeripheral] = [:]
     private var scannedDevicesUUIDs: [UUID] = []
-
+    private var scanTimer: Timer? = nil
+    
     //* Bluetooth runtime
     private var centralManager: CBCentralManager?
     private var obdAdapter: CBPeripheral?
     private var obdChannel: CBCharacteristic?
     private var channels: [String: CBCharacteristic] = [:]
-    public var connected: Bool = false
+    public var connected: Bool {
+        get {
+            // Check if the adapter is connected to central manager.
+            return (self.obdAdapter?.state ?? .disconnected) == .connected
+        }
+    }
     public var state: CBManagerState {
         get {
             return self.centralManager?.state ?? .unknown
         }
     }
-    public var isInitialized: Bool {
+    public var isPowredOn: Bool {
         get {
             return self.centralManager != nil && self.state == .poweredOn
         }
     }
-    public var isChannelOpened: Bool {
+    public var hasOpenWritingChannel: Bool {
         get {
-            return self.connected && !self.channels.isEmpty
+            // Check if adapter isn't connected
+            if !self.connected {
+                return false
+            }
+            // Check if channels are empty
+            if self.channels.isEmpty {
+                return false
+            }
+            // Check if channels has at least one channel with writting permissions
+            var hasWritingChannel = false
+            for (_, channel) in self.channels {
+                if channel.properties.contains(.write) || channel.properties.contains(.writeWithoutResponse) {
+                    hasWritingChannel = true
+                    break
+                }
+            }
+            return hasWritingChannel
         }
     }
     public var isScanning: Bool {
         get {
-            return self.isInitialized && self.centralManager?.isScanning ?? false
+            return self.isPowredOn && self.centralManager?.isScanning ?? false
         }
     }
     
     public init(delegate: BluetoothManagerDelegate?) {
         super.init()
         self.delegate = delegate
-        self.centralManager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
-        self.logger.log("BLEManager has been initialized. State is: \(self.state)")
+        self.centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
+        self.logger.log("init", "Bluetooth manager has been initialized.")
     }
     
     public func scanForDevices() {
         if !self.isScanning {
             self.centralManager?.scanForPeripherals(withServices: nil, options: nil)
+            // Initiate a scan cancel timer
+            self.scanTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.stopScan), userInfo: nil, repeats: false)
         }
     }
-
+    
+    @objc
+    public func stopScan() {
+        if self.isScanning && self.scanTimer != nil {
+            logger.log("stopScan", "Stopping bluetooth scan...")
+            // Stop scan
+            self.centralManager?.stopScan()
+            // Invalidate timer
+            self.scanTimer?.invalidate()
+            self.scanTimer = nil
+            logger.log("stopScan", "Scan stopped.")
+        }
+    }
+    
     /**
-    * Retrieves list of connected-to-system BLE devices and also serializes each device 
-    * into a JSON string to be sent back to FlutterApp through MethodChannel
-    */
+     * Retrieves list of connected-to-system BLE devices and also serializes each device
+     * into a JSON string to be sent back to FlutterApp through MethodChannel
+     */
     public func retrieveBoundedBluetoothDevicesSerialized() -> [String] {
         var devices: [String] = []
-        let devicesList: [CBPeripheral]  = self.centralManager?.retrievePeripherals(withIdentifiers: self.scannedDevicesUUIDs) ?? []
+        let devicesList: [CBPeripheral] = self.centralManager?.retrievePeripherals(withIdentifiers: self.scannedDevicesUUIDs) ?? []
         self.logger.log("Retrieved \(devicesList.count) devices.")
-        for bleDevice: CBPeripheral in devicesList {
-            if bleDevice.name != nil {
-                //* Add the device to bounded devices
-                self.boundedDevices[bleDevice.identifier.uuidString] = bleDevice
-            }
-            let deviceMapped = ["name": bleDevice.name ?? "unnamed device", "address": bleDevice.identifier.uuidString]
+        for bleDevice in devicesList {
+            //* Add the device to bounded devices
+            self.boundedDevices[bleDevice.identifier.uuidString] = bleDevice
+            let deviceName = bleDevice.name ?? bleDevice.identifier.uuidString
+            let deviceMapped = ["name": deviceName, "address": bleDevice.identifier.uuidString]
             if let jsonDevice = deviceMapped.serializeToJSON() {
                 devices.append(jsonDevice)
-                self.logger.log(jsonDevice)
             }
-            
         }
-        self.logger.log("retrieveBoundedBluetoothDevicesSerialized: \(devices)")
         return devices
     }
-
+    
     public func connect(target address: String?) async -> Bool {
-        if !self.isInitialized {
-            //* Not initialized. Report Bluetooth either poweredOff, unsupported or has error
-            self.logger.log("BluetoothManager isn't initialized. CurrentState: \(self.state)")
+        if !self.isPowredOn {
+            //* Not yet poweredOn. Report Bluetooth either poweredOff, unsupported or has error
+            self.logger.log("connect", "BluetoothManager isn't initialized. CurrentState: \(self.state)")
             return false
         }
         if self.connected {
             //* Already connected. No need to connect again
-            self.logger.log("Adapter is already connected")
+            self.logger.log("connect", "Adapter is already connected.. Aborted 'connect' operation.")
             return true
         }
         if self.obdAdapter == nil {
             if let address = address {
-                //* Retrieve device in bounded devices first
+                //* Retrieve device from bounded devices first
                 self.obdAdapter = self.boundedDevices[address]
                 //* Check again
                 if self.obdAdapter == nil {
-                    self.logger.log("Can't find the device at all. It may be out of range.")
+                    self.logger.log("connect", "Can't find the device at all. It may be out of range.")
                     // OBD adapter is either out of range or hasn't connected to system at first
                     return false
                 }
@@ -99,140 +132,137 @@ class BluetoothManager : NSObject {
                 return false
             }
         }
-        self.logger.log("Connecting to adapter...")
-        //* Connect to adapter
-        self.centralManager?.connect(obdAdapter!, options: nil)
+        // Connect to adapter
+        self.logger.log("connect", "Connecting to adapter...")
+        self.centralManager?.connect(obdAdapter!, options: [CBConnectPeripheralOptionNotifyOnConnectionKey: true])
         return self.connected
     }
-
+    
     /**
-    * Performs a characteristics discovery for each service
-    * discovered on connected device
-    *
-    * NOTE: Results are delegated to CBPeripheralDelegate
-    */
-    private func discoverCharacteristics(client: CBPeripheral?) {
-        if let services = client?.services {
-            self.logger.log("===== START SERVICE DISCOVERY =====")
+     * Performs a characteristics discovery for each service
+     * discovered on connected device
+     *
+     * NOTE: Results are delegated to CBPeripheralDelegate
+     */
+    private func discoverCharacteristics(device: CBPeripheral) {
+        if let services = device.services {
+            self.logger.log("discoverCharacteristics", "===== START SERVICE DISCOVERY =====")
             for service in services {
-                logger.log("\tDiscovering characteristics of service: \(service.uuid)")
+                logger.log("discoverCharacteristics", "Discovering characteristics of service: \(service.uuid)")
                 //* Discover our chars with our specific UUIDs
-                client?.discoverCharacteristics(nil, for: service)
+                device.discoverCharacteristics(nil, for: service)
             }
-            self.logger.log("===== END SERVICE DISCOVERY =====")
+            self.logger.log("discoverCharacteristics", "===== END SERVICE DISCOVERY =====")
         }
     }
-
+    
     public func send(dataToSend: String) async throws {
         //* Check if device is connected and at least only one characteristics has been discovered
-        guard self.isChannelOpened else {
-            self.logger.log("Neither adapter is connected nor at least one channel is opened.")
+        guard self.hasOpenWritingChannel else {
+            logger.log("send", "No channels are open for writing.")
             throw CommandExecutionError()
         }
         //* Get the bytes of the data to be sent encoded in utf8 format
-        guard let data = dataToSend.data(using: .utf8) else { 
-            self.logger.log("Can't send empty data.")
+        guard let data = dataToSend.data(using: .utf8) else {
+            self.logger.log("send", "Can't send empty data.")
             throw CommandExecutionError()
         }
         //* Send data adapter
         if let adapter = obdAdapter {
-            for channel in self.channels.keys {
-                if let chr = self.channels[channel] {
-                    if chr.properties.contains(.write) {
-                        self.logger.log("Writing with response: '\(String(data: data, encoding: .utf8) ?? "")' to channel: '\(channel)'")
-                        adapter.writeValue(data, for: chr, type: .withResponse)
-                        continue
-                    }
-                    if chr.properties.contains(.writeWithoutResponse) {
-                        self.logger.log("Writing without response: '\(String(data: data, encoding: .utf8) ?? "")' to channel: '\(channel)'")
-                        adapter.writeValue(data, for: chr, type: .withoutResponse)
-                        continue
-                    }
-                    self.logger.log("Charac: \(chr.uuid.uuidString) has value: \(ResponsePacket(payload: chr.value ?? Data()).decodePayload())")
+            for (_, channel) in self.channels {
+                self.logger.log("send", "Writing '\(String(data: data, encoding: .utf8) ?? "")' to channel: '\(channel)'")
+                if channel.properties.contains(.write) {
+                    adapter.writeValue(data, for: channel, type: .withResponse)
+                    continue
+                }
+                if channel.properties.contains(.writeWithoutResponse) {
+                    adapter.writeValue(data, for: channel, type: .withoutResponse)
+                    continue
                 }
             }
         }
     }
-
+    
     public func consumeNextResponse() -> ResponsePacket {
         let response = self.responseStation.consume()
-        self.logger.log("Consumed a response packet. Payload: '\(response.decodePayload())'")
+        self.logger.log("Consumed a response packet. Payload: '\(response.decodePayload().removeWhitespaces())'")
         return response
     }
-
+    
 }
 
 extension BluetoothManager: CBCentralManagerDelegate {
-
+    
     /** [DELEGATED] Called when central manager has discovered a new peripheral while performing a scan */
     func centralManager( _ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
-        let name = peripheral.name ?? "unnamed device"
         let address = peripheral.identifier
+        // Check whether the device is already in the list
+        if self.scannedDevicesUUIDs.contains(address) {
+            return
+        }
+        // Append the peripheral object to the list
+        let name = peripheral.name ?? address.uuidString
         self.scannedDevicesUUIDs.append(address)
-        self.logger.log("\nDiscovered BLE device: \(name) | \(address.uuidString)")
-        if name == OBDConstants.OBD_ADAPTER_NAME && !self.isChannelOpened {
-            self.logger.log("Found adapter at address: \(address.uuidString)")
+        self.logger.log("centralManager::didDiscover", "Discovered device: \(name) | \(address.uuidString)")
+        if name == OBDConstants.OBD_ADAPTER_NAME {
+            self.logger.log("centralManager::didDiscover", "Found adapter at address: \(address.uuidString)")
             // Stop scanning for devices
             self.centralManager?.stopScan()
             // Save a copy of adapter peripheral at runtime
             self.obdAdapter = peripheral
             self.boundedDevices[address.uuidString] = peripheral
-            // Connect to adapter
-            self.logger.log("Connecting to adapter...")
             //* Connect to adapter
             Task {
-                await self.connect(target: address.uuidString)
+                _ = await self.connect(target: address.uuidString)
             }
         }
     }
     
     /** [DELEGATED] Called when a central manager did updated its state */
     func centralManagerDidUpdateState( _ central: CBCentralManager) {
-        self.logger.log("BLE state has changed: \(self.state)")
+        self.logger.log("centralManager::centralManagerDidUpdateState", "Bluetooth state has changed: \(String(describing: BluetoothStates.BT_MAPPED_STATE[central.state]))")
     }
-
+    
     /** [DELEGATED] Called when a successful connection with the OBD adapter was established */
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         //* Discover services
-        self.connected = true
+        self.obdAdapter = peripheral
         self.obdAdapter?.delegate = self
         self.obdAdapter?.discoverServices(nil)
         self.delegate?.onAdapterConnected()
-        self.logger.log("Connected to \(peripheral.identifier.uuidString)")
+        self.logger.log("centralManager::didConnect", "Connected to \(peripheral.identifier.uuidString)")
     }
-
+    
     /** [DELEGATED] Called when the OBD adapter has disconnected or its connection was lost */
     func centralManager(central: CBCentralManager, didDisconnect peripheral: CBPeripheral, error: Error?) {
-        self.connected = false
         self.obdAdapter?.delegate = nil
-        self.logger.log("Disconnected from \(peripheral.identifier.uuidString) | Reason: \(String(describing: error))")
+        self.logger.log("centralManager::didDisconnect", "Disconnected from \(peripheral.identifier.uuidString) | Reason: \(String(describing: error))")
     }
-        
+    
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         if let error = error {
-            self.connected = false
-            self.logger.log("Error occurred while connecting: \(error)")
+            self.logger.log("centralManager::didFailToConnect", "Failed to connect to adapter. Reason: \(error)")
         }
     }
     
 }
 
 extension BluetoothManager : CBPeripheralDelegate {
-
+    
     /* [DELEGATED] Invoked when service discovery has completed */
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         if let error = error {
-            self.logger.log("Error occurred while discovering services: \(error)")
+            self.logger.log("centralManager::didDiscoverServices", "Error occurred while discovering services: \(error)")
             return
         }
         //* Discover Characteristics
-        self.discoverCharacteristics(client: peripheral)
+        self.discoverCharacteristics(device: peripheral)
     }
-
+    
     /* [DELEGATED] Invoked when characteristics discovery has completed */
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if let error = error {
-            self.logger.log("Error occurred while discovering characteristics: \(error)")
+            self.logger.log("centralManager::didDiscoverCharacteristicsFor", "Error occurred while discovering characteristics: \(error)")
             return
         }
         guard let characteristics = service.characteristics else { return }
@@ -240,30 +270,38 @@ extension BluetoothManager : CBPeripheralDelegate {
             peripheral.setNotifyValue(true, for: characteristic)
             let uuid = characteristic.uuid.uuidString
             self.channels[uuid] = characteristic
-            self.logger.log("Subscribed to characteristic: \(uuid)")
+            self.logger.log("centralManager::didDiscoverCharacteristicsFor", "Subscribed to channel: \(uuid)")
         }
     }
-
+    
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         if let error = error {
-            self.logger.log("Error reading response: \(error)")
+            self.logger.log("centralManager::didUpdateValueFor", "Error reading response: \(error)")
             return
         }
         //* Build a new packet for received data and push it to station
         if let response = characteristic.value {
             let responsePacket = ResponsePacket(payload: response)
-            self.responseStation.push(packet: responsePacket)
-            self.logger.log("Pushed a new packet to the station. Payload: (\(responsePacket.decodePayload())) | Station now has \(self.responseStation.queueSize) of \(self.responseStation.maxQueueSize) packets.")
+            // Check if response starts with standard fuel level response: 41 2F
+            let payload = responsePacket.decodePayload()
+            if payload.hasPrefix("41 2F") {
+                self.responseStation.push(packet: responsePacket)
+                self.logger.log("centralManager::didUpdateValueFor", "Pushed a new response to response station with new fuel level value.")
+            } else {
+                logger.log("centralManager::didUpdateValueFor", "Received response but non-numeric: \(responsePacket.decodePayload().removeWhitespaces())")
+            }
+            
         }
     }
-
+    
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
         let channelId = characteristic.uuid.uuidString
         if let error = error {
-            self.logger.log("Error writing to channel: \(channelId) | Reason: \(error)")
+            self.logger.log("centralManager::didWriteValueFor", "Error writing to channel: \(channelId) | Reason: \(error)")
+            return
         }
-        self.logger.log("Wrote something to channel: \(channelId)")
+        self.logger.log("centralManager::didWriteValueFor", "Wrote something to channel: \(channelId)")
     }
-
+    
 }
 
--- ios/Classes/Connection/Response.swift
+++ ios/Classes/Connection/Response.swift
@@ -3,42 +3,42 @@
 import Foundation
 
 class ResponsePacket {
-
+    
     let data: Data
-
+    
     var isEmpty: Bool {
         get {
             return self.data.isEmpty
         }
     }
-
+    
     init(payload data: Data) {
         self.data = data
     }
-
+    
     convenience init(payload dataString: String) {
         self.init(payload: dataString.data(using: .utf8) ?? Data())
     }
-
+    
     func decodePayload() -> String {
         return String(data: data, encoding: .utf8) ?? ""
     }
-
+    
     static func empty() -> ResponsePacket {
         return ResponsePacket(payload: "")
     }
-
+    
 }
 
 /**
-* Acts like a Queue with a consumer and a guard on accessing packets it holds.
-* This class simply has a Queue that holds a fixed size of 'ResponsePackets',
-* it also has a 'ResponseConsumer' that pops the most recent a response 
-* to the caller that want to consume it.
-* 
-*/
+ * Acts like a Queue with a consumer and a guard on accessing packets it holds.
+ * This class simply has a Queue that holds a fixed size of 'ResponsePackets',
+ * it also has a 'ResponseConsumer' that pops the most recent a response
+ * to the caller that want to consume it.
+ *
+ */
 class ResponseStation {
-
+    
     //* Runtime
     private var queue = [ResponsePacket]()
     public let maxQueueSize: Int
@@ -46,25 +46,27 @@ class ResponseStation {
         get {
             return queue.count
         }
-    } 
-    private var hasPackets: Bool {
-        get { return !queue.isEmpty }
     }
-
+    private var hasPackets: Bool {
+        get {
+            return !queue.isEmpty
+        }
+    }
+    
     init(queueSize maxQueueSize: Int = 50) {
         self.maxQueueSize = maxQueueSize
     }
-
+    
     public func push(packet responsePacket: ResponsePacket) {
         //* Remove the very first packet and push the new one
         if self.queueSize >= self.maxQueueSize {
-            self.consume()
+            _ = queue.removeFirst()
         }
         self.queue.append(responsePacket)
     }
-
+    
     public func consume() -> ResponsePacket {
         return self.hasPackets ? queue.removeFirst() : ResponsePacket.empty()
     }
-
+    
 }
